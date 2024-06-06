d3a665c28b4dac3f18d93f3e0592c734e52f66e4
Rei <bernardo2004costa@gmail.com>
2024-06-07 00:25:00 +0100

    Removed options array, but may need it later

--- gameGrid/game.js
+++ gameGrid/game.js
@@ -13,7 +13,7 @@ const winConditions = [
     [0, 4, 8],
     [2, 4, 6]
 ]
-let options = new Array(9).fill("");    
+//let options = new Array(9).fill("");    
 let currentPlayer = "X";
 let running = false;
 
@@ -31,19 +31,19 @@ function initialize() {
 // What happens when a cell is clicked
 function cellClicked() {
     const index = this.getAttribute("cellIndex");
-    console.log(index);
 
-    if (options[index] != "" || !running)
+    if (cells[index].textContent != "" || !running)  // options was here
         return;
 
-    updateCell(this, index);
+    updateCell(index);
     checkWinner();
+
+    //if (running) setTimeout(botsTurn, 500); // Add a slight delay for the bot's move
 }
 
 
-function updateCell(cell, index) {
-    options[index] = currentPlayer;
-    cell.textContent = currentPlayer;
+function updateCell(index) {
+    cells[index].textContent = currentPlayer;  // options was here
 }
 
 function changePlayer() {
@@ -57,9 +57,9 @@ function checkWinner() {
 
     for (let i = 0; i < winConditions.length; i++) {
         const condition = winConditions[i];
-        const c1 = options[condition[0]];
-        const c2 = options[condition[1]];
-        const c3 = options[condition[2]];
+        const c1 = cells[condition[0]].textContent;
+        const c2 = cells[condition[1]].textContent;
+        const c3 = cells[condition[2]].textContent;
 
         if (c1 == "" || c2 == "" || c3 == "") continue;
 
@@ -73,7 +73,7 @@ function checkWinner() {
         statusText.textContent = `${currentPlayer} ganhou!`;
         running = false;
     } 
-    else if (!options.includes("")) {
+    else if (![...cells].map(cell => cell.textContent).includes("")) {      // options was here
         statusText.textContent = `Empate!`;
         running = false;
     }
@@ -82,12 +82,34 @@ function checkWinner() {
 
 
 function restartGame() {
-    currentPlayer = "X";
-    options = new Array(9).fill("");    
+    currentPlayer = "X";  // options was here
     statusText.textContent = `Joga ${currentPlayer}!`
     cells.forEach(cell => cell.textContent = "");
     running = true;
 }
+
+/*function botsTurn() {
+    // First phase: check if there is any cell that must be filled
+    for (let i = 0; i < winConditions.length; i++) {
+        const condition = winConditions[i];
+        const c1 = options[condition[0]];
+        const c2 = options[condition[1]];
+        const c3 = options[condition[2]];
+
+        if (c1 == "X" && c2 == "X" && c3 == "") {
+            updateCell(cells[condition[2]], condition[2]);
+        }
+        else if (c1 == "X" && c2 == "" && c3 == "X") {
+            updateCell(cells[condition[1]], condition[1]);
+        }
+        else if (c1 == "" && c2 == "X" && c3 == "X") {
+            updateCell(cells[condition[0]], condition[10]);
+        }
+    }
+
+    // Second phase: go straight to the middle cell if unoccupied - greedy
+    if (options[4] == "") updateCell(cells[4], 4);
+}*/
 
 
 
