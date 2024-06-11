4d5295ad032bc7c74286c216c4bbb0f7d5121e22
Rei <bernardo2004costa@gmail.com>
2024-06-11 23:17:22 +0100

    Selection for 10 seconds.

--- selection.js
+++ selection.js
@@ -1,10 +1,38 @@
-document.getElementById("playerX").addEventListener("click", () => choosePlayer("X"));
-document.getElementById("playerO").addEventListener("click", () => choosePlayer("O"));
+let x = document.getElementById("playerX");
+let o = document.getElementById("playerO");
+
+x.addEventListener("click", () => choosePlayer("X"));
+o.addEventListener("click", () => choosePlayer("O"));
 
 function choosePlayer(player) {
     localStorage.setItem("jogador", player);
     window.location.href = "actualGame/game.html";
 }
+
+
+
+const solo = document.getElementById("solo");
+const bot = document.getElementById("bot");
+const inf = document.getElementById("infinite");
+
+function pressDown(element) {
+    document.addEventListener("DOMContentLoaded", () => {
+    
+        element.addEventListener('click', () => {
+            element.classList.add('button-pressed');
+    
+            setTimeout(() => {
+                element.classList.remove('button-pressed');
+            }, 10000); // 10 seconds
+        });
+    });
+}
+
+pressDown(solo);
+pressDown(bot);
+pressDown(inf);
+
+
 
 
 
