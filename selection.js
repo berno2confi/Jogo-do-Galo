c78c9b72b27bc5955a464e9f0bc94efded967f68
Rei <bernardo2004costa@gmail.com>
2024-06-16 23:18:53 +0100

    Finished logic for button pressing.

--- selection.js
+++ selection.js
@@ -6,32 +6,56 @@ const inf = document.getElementById("infinite");
 const play = document.getElementById("play");
 
 let pressedButtons = {
-    x: false,
-    o: false,
+    playerX: false,
+    playerO: false,
     solo: false,
     bot: false,
     infinite: false,
 };
 
-/*x.addEventListener("click", () => choosePlayer("X"));
-o.addEventListener("click", () => choosePlayer("O"));
-
-function choosePlayer(player) {
-    localStorage.setItem("jogador", player);
-    sendPressedButtons();
-    window.location.href = "actualGame/game.html";
-}*/
-
 function pressDown(element, key) {
     element.addEventListener('click', () => {
-        element.classList.add('button-pressed');
-        pressedButtons[key] = true;
+        // X releases O. O releases X, and solo, if pressed.
+        if (key == "playerX") {
+            if (pressedButtons.playerO)
+                releaseButton(o, "playerO");
+        }
+        else if (key == "playerO") {
+            if (pressedButtons.playerX) {
+                releaseButton(x, "playerX");
+                if (pressedButtons.solo)
+                    releaseButton(solo, "solo");
+            }
+        }
+        // Solo is always paired with X, and releases bot and O if pressed. Bot releases solo.
+        if (key == "solo") {
+            if (pressedButtons.bot)
+                releaseButton(bot, "bot");
+            if (pressedButtons.playerO)
+                releaseButton(o, "playerO");
+            pressButton(x, "playerX");
+        }
+        else if (key == "bot") {
+            if (pressedButtons.solo)
+                releaseButton(solo, "solo");
+        }
 
-        setTimeout(() => {
-            element.classList.remove('button-pressed');
-            pressedButtons[key] = false;
-        }, 10000); // 10 seconds
+        pressButton(element, key);
     });
+}
+
+function pressButton(element, key) {
+    if (pressedButtons[key]) {
+        releaseButton(element, key);
+        return;
+    }
+    element.classList.add('button-pressed');
+    pressedButtons[key] = true;
+}
+
+function releaseButton(element, key) {
+    element.classList.remove('button-pressed');
+    pressedButtons[key] = false;
 }
 
 pressDown(x, "playerX");
@@ -44,4 +68,3 @@ play.addEventListener("click", () => {
     localStorage.setItem("pressedButtons", JSON.stringify(pressedButtons));
     window.location.href = "actualGame/game.html";
 });
-
